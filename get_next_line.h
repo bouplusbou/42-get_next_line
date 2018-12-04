7e9099ff9ec4572170bba655b846bcbbe9efd4a9
Baptiste BOUCHER <bboucher@e1r1p13.42.fr>
2018-12-04 11:08:28 +0100

    save GNL

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: bboucher <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/20 11:18:14 by bboucher          #+#    #+#             */
-/*   Updated: 2018/12/04 09:25:31 by bboucher         ###   ########.fr       */
+/*   Updated: 2018/12/04 10:24:46 by bboucher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 1
+# define BUFF_SIZE 5
 # include <sys/types.h>
 # include <sys/uio.h>
 # include <unistd.h>
--- tests/main.c
+++ tests/main.c
@@ -6,42 +6,34 @@
 /*   By: bboucher <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/20 14:06:25 by bboucher          #+#    #+#             */
-/*   Updated: 2018/12/04 09:14:36 by bboucher         ###   ########.fr       */
+/*   Updated: 2018/12/04 10:49:15 by bboucher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-int	main(int c, char **v)
+int			main(int c, char **v)
 {
 	int		fd;
+	int		ret;
 	int		i;
 	char	*line;
 
-	fd = open(v[1], O_RDONLY);
-	i = 0;
-	line = ft_strnew(0);
-	while (i < ft_atoi(v[2]))
+	i = 1;
+	while (i < c)
 	{
-		//		get_next_line(fd, &line);
-		printf("%d line: %s\n", get_next_line(fd, &line), line);
+		if ((fd = open(v[i], O_RDONLY)) == -1)
+			printf("ERROR\n");
+		while ((ret = get_next_line(fd, &line)) > 0)
+		{
+			printf("%d - |%s|\n", ret, line);
+			free(line);
+		}
 		i++;
 	}
-	ft_strdel(&line);
-	return (c);
-}
-/*
-int main(int ac, char **av)
-{
-	char *line;
-	int fd;
-
-	(void)ac;
-	(void)av;
-	fd = open(av[1], O_RDONLY);
-	while (get_next_line(fd, &line) == 1)
-		printf("%s\n", line);
+	while (1)
+		c = 1;
 	return (0);
 }
-*/
+
